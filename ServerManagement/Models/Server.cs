83f373c3a0fedf5e7587bb9c70efc88e29f50b6f
Egor Quenum <georgequenum@yahoo.com>
2024-09-21 00:07:30 +0400

    Add server model

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,17 @@
+namespace ServerManagement.Models;
+
+public class Server
+{
+    public int Id { get; set; }
+    public bool IsOnline { get; set; }
+    public string? Name { get; set; }
+    public string? City { get; set; }
+
+    public Server()
+    {
+        IsOnline = new Random().Next(0, 2) == 0;
+    }
+
+    public string GetStatusString() =>
+        IsOnline ? "Online" : "Offline";
+}
