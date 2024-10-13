b8040490840074582dd604214f3a547f31eb3915
Egor Quenum <georgequenum@yahoo.com>
2024-10-14 01:48:03 +0400

    Add server update validation

--- ServerManagement/Models/Server.cs
+++ ServerManagement/Models/Server.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ServerManagement.Models;
 
 public class Server
 {
+    [Required]
+    [Range(0, int.MaxValue)]
     public int Id { get; set; }
     public bool IsOnline { get; set; }
+
+    [Required(AllowEmptyStrings = false)]
+    [MaxLength(20)]
     public string Name { get; set; }
+
+    [Required(AllowEmptyStrings = false)]
+    [MaxLength(20)]
     public string City { get; set; }
 
     public Server()
